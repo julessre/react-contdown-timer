f80676c2f98a58b0753d7951dee3d3e05bbb2cfd
Julia <jules.rechi@gmail.com>
2024-01-19 14:25:21 +0100

    Setting up the basic countdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import './App.css';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+  const [counter, setCounter] = useState(60);
+
+  useEffect(() => {
+    const timer =
+      counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+  }, [counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Countdown to summer:
+      <br />
+      {counter}
     </div>
   );
 }
+
+// useEffect(<function>, <dependency>)
